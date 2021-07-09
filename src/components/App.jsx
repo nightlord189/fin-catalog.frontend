ac588ec41d5240f42c92101ab23d1b2c9a945d0f
Alexey Buravov <night.lord.189@gmail.com>
2021-07-10 02:50:50 +0600

    import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,37 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
 
 const App = () => {
     return (
-        <div className="App">
+        <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Депозиты</Link>
+              </li>
+              <li>
+                <Link to="/about">О проекте</Link>
+              </li>
+            </ul>
+          </nav>
+          <div className="App">
             <h1>Test1</h1>
+          </div> 
+          <Switch>
+            <Route path="/about">
+            </Route>
+            <Route path="/">
+            </Route>
+          </Switch>
         </div>
+      </Router>
     );
 }
 
